733c92b6e7d8f9f7ff4803847e1a5b7180f26c28
turkmuhendisi <1turkmuhendisi@gmail.com>
2024-05-21 14:41:36 +0300

    -JWT implementation

--- src/main/java/com/turanpay/account/model/Customer.kt
+++ src/main/java/com/turanpay/account/model/Customer.kt
@@ -2,6 +2,7 @@ package com.turanpay.account.model
 
 import jakarta.persistence.*
 import org.hibernate.annotations.GenericGenerator
+import java.time.LocalDateTime
 
 @Entity
 data class Customer(
@@ -15,16 +16,27 @@ data class Customer(
     val phone: String?,
     val password: String?,
 
+    @ElementCollection(targetClass = Role::class, fetch = FetchType.EAGER)
+    @JoinTable(name = "authorities", joinColumns = [JoinColumn(name = "user_id")])
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    val authorities: Set<Role>?,
+
+    val creationDate: LocalDateTime?,
+
     @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
     val accounts: Set<Account>? = HashSet()
 ) {
-    constructor(name: String, surname: String, email: String, phone: String, password: String) : this(
+
+    constructor(name: String, surname: String, email: String, phone: String, password: String, creationDate: LocalDateTime) : this(
         "",
         name,
         surname,
         email,
         phone,
-        password
+        password,
+        authorities = setOf(Role.ROLE_USER),
+        creationDate = creationDate
     )
 
     override fun equals(other: Any?): Boolean {
@@ -39,6 +51,7 @@ data class Customer(
         if (email != other.email) return false
         if (phone != other.phone) return false
         if (password != other.password) return false
+        if (authorities != other.authorities) return false
         if (accounts != other.accounts) return false
 
         return true
@@ -51,9 +64,11 @@ data class Customer(
         result = 31 * result + (email?.hashCode() ?: 0)
         result = 31 * result + (phone?.hashCode() ?: 0)
         result = 31 * result + (password?.hashCode() ?: 0)
+        result = 31 * result + (authorities?.hashCode() ?: 0)
         return result
     }
 }
+
 
 
 
---
+++ src/main/java/com/turanpay/account/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.turanpay.account.security;
+
+import com.turanpay.account.service.CustomerService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomerService customerService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter, CustomerService customerService, PasswordEncoder passwordEncoder) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.customerService = customerService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(x -> x
+                        .requestMatchers("/auth/welcome/**",
+                                "/auth/customer-register/**",
+                                "/auth/generateToken/**",
+                                "/auth/create-account/**",
+                                "/auth/iban/**").permitAll()
+                        .requestMatchers("/auth/user/**", "/auth/profile/**").hasRole("USER")
+                        .requestMatchers("/auth/admin/**").hasRole("ADMIN")
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(customerService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
